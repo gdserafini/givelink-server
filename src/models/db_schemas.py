f5d4df02cdd1e0041ad5bf952bac771bc4ede2da
gdserafini <gdserafini@outlook.com>
2025-03-24 22:47:24 -0300

    update -> db modeling and migration with alembic

---
+++ src/models/db_schemas.py
@@ -0,0 +1,103 @@
+from datetime import datetime
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, registry
+from enum import Enum
+from datetime import datetime
+
+
+table_registry = registry()
+
+
+@table_registry.mapped_as_dataclass
+class UserModel:
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True, init=False
+    )
+    username: Mapped[str] = mapped_column(
+        unique=True, nullable=False
+    )
+    password: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(
+        nullable=False, unique=True
+    )
+    avatar_url: Mapped[str] = mapped_column(nullable=True)
+    role_id: Mapped[int] = mapped_column(
+        ForeignKey('roles.id'), nullable=False
+    )
+
+
+class RoleEnum(str, Enum):
+    ADMIN = 'admin'
+    USER = 'user'
+
+
+class RoleEnumLevel(int, Enum):
+    ADMIN = 1
+    USER = 2
+
+
+@table_registry.mapped_as_dataclass
+class RolesModel:
+    __tablename__ = 'roles'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    role: Mapped[str] = mapped_column(
+        unique=True, nullable=False
+    )
+    level: Mapped[int] = mapped_column(nullable=False)
+
+
+@table_registry.mapped_as_dataclass
+class DonorModel:
+    __tablename__ = 'donors'
+    id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True, init=False
+    )
+    name: Mapped[str] = mapped_column(nullable=False)
+    avatar_url: Mapped[str] = mapped_column(nullable=True)
+    cpf_cnpj: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id'), nullable=False
+    )
+
+
+@table_registry.mapped_as_dataclass
+class InstitutionModel:
+    __tablename__ = 'institutions'
+    id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True, init=False
+    )
+    name: Mapped[str] = mapped_column(nullable=False)
+    sector: Mapped[str] = mapped_column(nullable=False)
+    avatar_url: Mapped[str] = mapped_column(nullable=True)
+    cnpj: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id'), nullable=False
+    )
+
+
+class PaymentMethod(str, Enum):
+    PIX = 'pix'
+    TED = 'ted'
+    BOLETO = 'boleto'
+
+
+@table_registry.mapped_as_dataclass
+class DonationModel:
+    __tablename__ = 'donations'
+    id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True, init=False
+    )
+    amount: Mapped[float] = mapped_column(nullable=False)
+    date: Mapped[datetime] = mapped_column(
+        nullable=False, server_default=func.now(), init=False
+    )
+    payment_method: Mapped[str] = mapped_column(
+        nullable=False
+    )
+    donor_id: Mapped[int] = mapped_column(
+        ForeignKey('donors.id'), nullable=False
+    )
+    institution_id: Mapped[int] = mapped_column(
+        ForeignKey('institutions.id'), nullable=False
+    )
