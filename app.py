9a8cef4053787592af5f9445594bd56d55f559c2
gdserafini <gdserafini@outlook.com>
2025-04-29 19:00:42 -0300

    add -> donation models

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from src.controllers.user_controller import router as user_router
 from src.controllers.auth_controller import router as auth_router
 from src.controllers.donor_controller import router as donor_router
 from src.controllers.institution_controller import router as institution_router
+from src.controllers.donation_controller import router as donation_router
 from src.service.session import setup_db
 import sys
 
@@ -49,6 +50,7 @@ app.include_router(user_router)
 app.include_router(auth_router)
 app.include_router(donor_router)
 app.include_router(institution_router)
+app.include_router(donation_router)
 
 
 @app.get("/")
